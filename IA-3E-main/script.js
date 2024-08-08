9f1a2d73a815a8b48fbf7f71d5d3765434196b25
duplaxs <110491486+duplaxs@users.noreply.github.com>
2024-08-08 07:44:11 -0300

    Add files via upload

---
+++ IA-3E-main/script.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector('caixa-principal');
+const caixaPergunta = document.querySelector('caixa-pergunta');
+const caixaAlternativa = document.querySelector('caixa-alternativa');
+const caixaResultado= document.querySelector('caixa-resultado');
+const pergunta=[
+    {
+        enunciado: "pergunta 1",
+        alternativa:[ "alterntiva 1"," alternativa 2",]          
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativa:[ "alterntiva 1"," alternativa 2"]        
+    },
+    {
+        enunciado: "pergunta 3",
+        alternativa:[ "alterntiva 1"," alternativa 2"]       
+    },
+]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    
+}
+
